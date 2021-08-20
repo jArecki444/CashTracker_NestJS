3c4968d1d49ac1c6246bdf29ff4b3fc2fd54b1c3
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-08-20 18:53:53 +0200

    Sign in without jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +14,20 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const existingUser = await this.usersRepository.findOne({ username });
+
+    if (
+      existingUser &&
+      (await bcrypt.compare(password, existingUser.password))
+    ) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Invalid username or password');
+    }
   }
 }
